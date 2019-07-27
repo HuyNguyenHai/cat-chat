576e85a0288ef211d9b1a445eef3383ac492a384
HuyNguyenHai <nguyenhaihuyyy@gmail.com>
2019-07-28 00:21:36 +0700

    [1.0.0] Connect and Create new Mongodb items

--- src/server.js
+++ src/server.js
@@ -1,14 +1,29 @@
+require('dotenv').config()
 import express from "express";
+import ConnectDB from "./config/connectDB";
+import ContactModel from "./models/contact.model";
+
 let app = express();
 
-let hostname = "localhost";
-let port = 8000;
+ConnectDB();
 
-app.get("/helloworld", (req, res) => {
-  res.send("Hello world!!!!!");
+let hostname = process.env.APP_HOST;
+let port = process.env.APP_PORT;
+
+app.get("/test-database", async (req, res) => {
+  try {
+    let item = {
+      userId: "123134123",
+      contactId: "e23423df"
+    };
+
+    let contact = await ContactModel.createNew(item);
+    res.send(contact);
+  } catch (error) {
+    console.log(error);
+  }
 });
 
 app.listen(port, hostname, () => {
   console.log(`Server is started at ${hostname}:${port}/`);
 });
-
