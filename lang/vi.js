a81508c024d59223bc033f47eaa3bc4bfa188fd9
HuyNguyenHai <nguyenhaihuyyy@gmail.com>
2019-07-28 22:42:44 +0700

    Add register feature

---
+++ lang/vi.js
@@ -0,0 +1,6 @@
+export const transValidation = {
+  email_incorrect: "Hãy nhập lại một Emeow hợp lệ nhé! Meowww!",
+  gender_incorrect: "Bạn không nằm trong 2 loại giới tính  này ư?",
+  password_incorrect: "Hãy nhập mật khẩu với ít nhất 8 ký tự.",
+  password_confirmation_incorrect: "Mật khẩu không khớp."
+}
--- src/controllers/authController.js
+++ src/controllers/authController.js
@@ -1,12 +1,27 @@
+import {validationResult} from "express-validator/check";
+import { json } from "body-parser";
+
 let getLoginRegister = (req, res) => {
   res.render("auth/master");
 };
 
-let getLogout = () => {
-  res.render("auth/master")
+let postRegister = (req, res) => {
+  let errorArray = [];
+
+  let validationErrors = validationResult(req);
+
+  if(!validationErrors.isEmpty()){
+    let errors = Object.values(validationErrors.mapped());
+    errors.forEach(element => {
+      errorArray.push(element);
+    });
+    
+    console.log(errorArray);
+  }
+  
 }
 
 module.exports = {
   getLoginRegister: getLoginRegister,
-  getLoout: getLogout
+  postRegister: postRegister
 };
