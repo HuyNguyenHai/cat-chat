f81e7854bc3e0969239455f63f1f87abbe4882df
HuyNguyenHai <nguyenhaihuyyy@gmail.com>
2019-09-22 22:59:17 +0700

    get review message

--- src/helpers/clientsHelper.js
+++ src/helpers/clientsHelper.js
@@ -1,5 +1,12 @@
-let bufferToBase64 = (bufferFrom) => {
+import moment from 'moment';
+
+export let bufferToBase64 = (bufferFrom) => {
     return Buffer.from(bufferFrom).toString("base64");
 };
 
-module.exports = bufferToBase64;
+export let convertTimestampToHumanTime = (timestamp) => {
+    if(!timestamp) {
+        return "";
+    }
+    return moment(timestamp).locale("vi").startOf("seconds").fromNow();
+}
