09ef91912a7849060112db88a82dfe085e9cfb0f
HuyNguyenHai <nguyenhaihuyyy@gmail.com>
2019-09-25 11:17:00 +0700

    send-text-and-emoji(not-realtime)

--- src/public/js/sendTextAndEmoji.js
+++ src/public/js/sendTextAndEmoji.js
@@ -1,6 +1,7 @@
 function sendTextAndEmoji(divId) {
     $('.emojionearea').unbind('keyup').bind('keyup', function(event) {
         if(event.which == 13) {
+            $('.emojionearea-editor').html("");
             let targetId = $(`#write-chat-${divId}`).data('chat');
             let messageText = $(`#write-chat-${divId}`).val();
 
@@ -13,13 +14,36 @@ function sendTextAndEmoji(divId) {
                 messageValue: messageText
             }
 
-            if  ($(`#write-chat-${divId}`).hasClass('chat-in-group')) {
+            if($(`#write-chat-${divId}`).hasClass('chat-in-group')) {
                 dataTextEmojiForSend.isChatGroup = true;
             }
 
             $.post("/message/add-new-message", dataTextEmojiForSend,
                 function (data) {
-                    
+                    let newMessageElement = $(`<div class="convert-emoji bubble me data-mess-id="${data.message._id}"></div>`);
+                    if(dataTextEmojiForSend.isChatGroup) {
+                        newMessageElement.html(
+                            `<img src="/images/users/${data.message.sender.avatar}" class="avatar-small" title="${data.message.sender.name}">`
+                        );
+                        newMessageElement.html(emojione.toImage(data.message.text));
+                    } else {
+                        newMessageElement.html(emojione.toImage(data.message.text));
+                    }
+                    increaseNumberMessageGroup(targetId);
+                    $(`.right .chat[data-chat = ${divId}]`).append(newMessageElement);
+                    $(`.right .chat[data-chat = ${divId}]`).scrollTop($(`.right .chat[data-chat = ${divId}]`)[0].scrollHeight);
+
+                    $(`.person[data-chat = ${divId}]`).find('span.time').html(moment(data.message.createAt).locale('vi').startOf("seconds").fromNow());
+                    $(`.person[data-chat = ${divId}]`).find('span.preview').html(emojione.toImage(dataTextEmojiForSend.messageValue));
+
+                    $(`.person[data-chat = ${divId}]`).on('click.moveConversationToTop', function() {
+                        let dataMove = $(this).parent();
+                        dataMove.closest('ul').prepend(dataMove);
+                        $(this).off('click.moveCOnversationToTop');
+                    });
+                    $(`.person[data-chat = ${divId}]`).click();
+
+                    //emmit realtime here
                 }
             ).fail(function(response) {
                 alertify.notify(response.responseText, "error", 7);
