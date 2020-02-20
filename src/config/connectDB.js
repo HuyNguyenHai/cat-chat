f46ab6acf3348e80944b958c50b7e598fc3d3d5c
HuyNguyenHai <nguyenhaihuyyy@gmail.com>
2020-02-20 17:22:24 +0700

    fixbug

--- src/config/connectDB.js
+++ src/config/connectDB.js
@@ -8,8 +8,15 @@ import bluebird from "bluebird";
  let connectDB = () => {
     mongoose.Promise = bluebird;
 
-    let URI = `${process.env.DB_CONNECTION}://${process.env.DB_HOST}:${process.env.DB_PORT}/${process.env.DB_NAME}`;
-    return mongoose.connect(URI, {useMongoClient: true});
+   //  let URI = `${process.env.DB_CONNECTION}://${process.env.DB_HOST}:${process.env.DB_PORT}/${process.env.DB_NAME}`;
+   let URI = `${process.env.DB_CONNECTION_STRING}`
+    return mongoose.connect(URI, {
+      useCreateIndex: true,
+		useNewUrlParser: true,
+		useUnifiedTopology: true,
+      dbName: 'cat-chat-vn',
+      useFindAndModify: false
+      });
  };
 
 module.exports = connectDB;
