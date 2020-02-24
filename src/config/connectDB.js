551c6bf7aa0a3027f93a14832f056bb1b96ab045
HuyNguyenHai <nguyenhaihuyyy@gmail.com>
2020-02-24 23:53:40 +0700

    fixbug

--- src/config/connectDB.js
+++ src/config/connectDB.js
@@ -9,7 +9,8 @@ import bluebird from "bluebird";
     mongoose.Promise = bluebird;
 
    //  let URI = `${process.env.DB_CONNECTION}://${process.env.DB_HOST}:${process.env.DB_PORT}/${process.env.DB_NAME}`;
-   let URI = `${process.env.DB_CONNECTION_STRING}`
+   // let URI = `${process.env.DB_CONNECTION_STRING}`
+   let URI = `mongodb+srv://admin:1@cluster0-xe0bx.mongodb.net/test?retryWrites=true&w=majority`
     return mongoose.connect(URI, {
       useCreateIndex: true,
 		useNewUrlParser: true,
